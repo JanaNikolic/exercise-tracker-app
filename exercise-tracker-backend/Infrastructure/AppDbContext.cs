705539575af39b9488e8326b9a0af0a87dde5f30
JanaNikolic <jananikolic25@gmail.com>
2026-06-16 17:21:26 +0200

    Backend database setup with user model

---
+++ exercise-tracker-backend/Infrastructure/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
