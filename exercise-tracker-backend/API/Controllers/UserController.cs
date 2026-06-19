f54bd768adc34a74547a332ee3c675cd3b9654a7
JanaNikolic <jananikolic25@gmail.com>
2026-06-19 12:46:48 +0200

    feat(backend): add user profile endpoint

--- exercise-tracker-backend/API/Controllers/UserController.cs
+++ exercise-tracker-backend/API/Controllers/UserController.cs
@@ -54,5 +54,16 @@ namespace API.Controllers
             var response = _mapper.Map<LoginResponseDto>(token);
             return Ok(response);
         }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<IActionResult> GetProfileAsync()
+        {
+            long? userId = User.GetUserId(); 
+            if (userId == null) return Unauthorized();
+
+            var profile = await _userService.GetByIdAsync(userId.Value);
+            return Ok(profile);
+        }
     }
 }
